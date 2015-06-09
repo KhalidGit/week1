c6b03510556588ec53dc46fe621cb581e276a641
Khalid Hamza <Khalid_hamza@utexas.edu>
2015-06-08 22:12:21 -0500

    Update and rename day1ObjectsSolutions.js to day1Questions.js

---
+++ day1Questions.js
@@ -0,0 +1,41 @@
+// Telegraph Academy Prep-Week 1-Day 1-Objects
+  
+  //What is an object? 
+  // --- An object is a data structure, variable, function, or a collection of properties that refer to an instance of a class. 
+
+  //When can you only use dot notation and not bracket notation?
+  // --- Dot notation can be used exclusively for string key values, while bracket notation can be used for all other primitives. 
+
+  //What is the difference between dot and bracket notation?
+  // --- Dot notation can only take strings.??? Look at the slide The Rules
+
+  //When can you only use brackets and not dot?
+  // --- You can only use only bracket notation when there is a non-string data type as the key.
+
+  //How do you add a property with a key that contains special characters?
+  // --- Using brackets. box["^&*"] = "testing 123"
+
+  //How do you add a property with a key that is stored in a variable?
+	// --- 
+  // var box = {}
+  //var key = "Hello"
+  //box[key] = "world
+  
+  //How do you add a property whose key and value are stored in different variables?
+	// ---
+  // var box = {}
+  //var key = "Hello"
+  //var sec = "world"
+  //box[key] = sec
+  
+  //How do you access an object that is inside another object?
+  // ---
+  
+  
+  //How do we loop through objects to access the values?
+	// --- for (var key in box){
+  					console.log(key); // console.log(box[key]);
+  				}
+  
+  //How do you create an object that is nested inside another object?
+	// ---
